99af29002d415e94ca75111b7074f6a63fba06ec
GU-JQR <20210312213@squ.edu.cn>
2024-04-18 13:57:09 +0800

    2024.4.18113

--- vite/vuecli/src/App.vue
+++ vite/vuecli/src/App.vue
@@ -1,28 +1,9 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    来自app.vue的信息
   </div>
 </template>
+<!-- template标签内只能有一个根标签 -->
+<script></script>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
