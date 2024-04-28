25563d040e9dd67bc5961ce15a54d874659e0266
GU-JQR <20210312213@squ.edu.cn>
2024-04-28 14:20:40 +0800

    2024.4.28.14

--- vite/vuecli2/src/mall/LoginReg.vue
+++ vite/vuecli2/src/mall/LoginReg.vue
@@ -5,22 +5,22 @@
             <div slot="header" class="clearfix " style="text-align: center;">****系统</div>
             <div>内容</div>
             <el-tabs stretch>
-                <el-tab-pane label="登录" name="login">这是登录的界面</el-tab-pane>
+                <el-tab-pane label="登录" name="login">
+                    <el-form :model="loginForm" status-icon :rules="rules" ref="loginForm" label-width="100px"
+                        class="demo-ruleForm">
+                        <el-form-item label="用户名" prop="pass">
+                            <el-input type="text" v-model="loginForm.userName" autocomplete="off"></el-input>
+                        </el-form-item>
+                        <el-form-item label="密码" prop="checkPass">
+                            <el-input type="password" v-model="loginForm.userCode" autocomplete="off"></el-input>
+                        </el-form-item>
+                        <el-form-item style="text-align: center;">
+                            <el-button type="primary" @click="submitForm('loginForm')">提交</el-button>
+                        </el-form-item>
+                    </el-form>
+                </el-tab-pane>
                 <el-tab-pane label="注册" name="register">这是注册的界面</el-tab-pane>
             </el-tabs>
-            <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" label-width="100px"
-                class="demo-ruleForm">
-                <el-form-item label="用户名" prop="pass">
-                    <el-input type="password" v-model="ruleForm.pass" autocomplete="off"></el-input>
-                </el-form-item>
-                <el-form-item label="密码" prop="checkPass">
-                    <el-input type="password" v-model="ruleForm.checkPass" autocomplete="off"></el-input>
-                </el-form-item>
-                <el-form-item>
-                    <el-button type="primary" @click="submitForm('ruleForm')">提交</el-button>
-                    <el-button @click="resetForm('ruleForm')">重置</el-button>
-                </el-form-item>
-            </el-form>
         </el-card>
     </div>
 </template>
@@ -29,7 +29,10 @@
 export default {
     data() {
         return {
-            ruleForm: {}
+            loginForm: {
+                userName: '',
+                userCode: '',
+            }
         }
     }
 }
