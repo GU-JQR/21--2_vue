bb761f8d64065942a16bd7a73b762e681a201da5
GU-JQR <20210312213@squ.edu.cn>
2024-04-30 08:19:39 +0800

    2024.4.30.8

--- vite/vuecli2/src/mall/LoginReg.vue
+++ vite/vuecli2/src/mall/LoginReg.vue
@@ -4,7 +4,7 @@
         <el-card class="boxCard">
             <div slot="header" class="clearfix " style="text-align: center;">****系统</div>
             <div>内容</div>
-            <el-tabs stretch>
+            <el-tabs stretch v-model="activeName" @tab-click="handleClick">
                 <el-tab-pane label="登录" name="login">
                     <el-form :model="loginForm" status-icon :rules="rules" ref="loginForm" label-width="100px"
                         class="demo-ruleForm">
@@ -19,7 +19,24 @@
                         </el-form-item>
                     </el-form>
                 </el-tab-pane>
-                <el-tab-pane label="注册" name="register">这是注册的界面</el-tab-pane>
+                <el-tab-pane label="注册" name="register">
+                    <el-form :model="RegisterFrom" status-icon :rules="rules" ref="RegisterFrom" label-width="100px"
+                        class="demo-ruleForm">
+                        <el-form-item label="用户名" prop="userName">
+                            <el-input type="text" v-model="RegisterFrom.userName" autocomplete="off"></el-input>
+                        </el-form-item>
+                        <el-form-item label="密码" prop="userCode">
+                            <el-input type="password" v-model="RegisterFrom.userCode" autocomplete="off"></el-input>
+                        </el-form-item>
+                        <el-form-item label="确认密码" prop="configUserCode">
+                            <el-input type="password" v-model="RegisterFrom.configUserCode"
+                                autocomplete="off"></el-input>
+                        </el-form-item>
+                        <el-form-item style="text-align: center;">
+                            <el-button type="primary" @click="submitForm('RegisterFrom')">提交</el-button>
+                        </el-form-item>
+                    </el-form>
+                </el-tab-pane>
             </el-tabs>
         </el-card>
     </div>
@@ -36,7 +53,6 @@ export default {
                 callBack()
             }
         }
-
         var validateUserCode = (rule, value, callBack) => {
             if (value === '') {
                 callBack(new Error('请输入密码'))
@@ -44,10 +60,24 @@ export default {
                 callBack()
             }
         }
+        var validateConfigUserCode = (rule, value, callBack) => {
+            if (value === '') {
+                callBack(new Error('请输入确认密码'))
+            } else {
+                callBack()
+            }
+        }
         return {
+            activeName: "login",//默认显示的选项卡
+            currentTab: "login",
             loginForm: {
                 userName: '',
                 userCode: '',
+            },
+            RegisterFrom: {
+                userName: '',
+                userCode: '',
+                configUserCode: '',
             },
             rules: {
                 userName: [
@@ -55,6 +85,9 @@ export default {
                 ],
                 userCode: [
                     { validator: validateUserCode, trigger: 'blur' }
+                ],
+                configUserCode: [
+                    { validator: validateConfigUserCode, trigger: 'blur' }
                 ]
             }
         }
@@ -63,12 +96,20 @@ export default {
         submitForm(formName) {
             this.$refs[formName].validate((valid) => {
                 if (valid) {
-                    console.log('submit!');
+                    if (this.currentTab == 'login') {
+                        alert('登录中');
+                    } else if (this.currentTab == 'register') {
+                        alert('注册中');
+                    }
                 } else {
                     console.log('error submit!!');
                     return false;
                 }
             });
+        },
+        handleClick(tab, event) {
+            console.log(tab.name);
+            this.currentTab = tab.name;
         }
     }
 }
